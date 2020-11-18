ae621468bec40a63c1c9e5a6ac8524590745e82a
Yura Rekechuk <okgo0801@gmail.com>
2020-11-18 22:50:07 +0200

    [+] landing layout

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,29 +1,30 @@
-
 export default {
-  mode: 'universal',
   /*
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'SLP SHOT, INC Steve Lee Parsons - Vintage and modern guns, Luxury Mechanical Watches',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'SLP SHOT, INC Steve Lee Parsons - Vintage and modern guns, Luxury Mechanical Watches' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/img/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/img/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/img/favicon-16x16.png' },
+      { rel: 'icon', type: 'image/x-icon', href: '/img/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&family=Montserrat:ital,wght@0,400;0,500;0,700;0,800;1,300&display=swap' }
     ]
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: '#0C1B42' },
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: ['~/assets/styles/main'],
   /*
   ** Plugins to load before mounting the App
   */
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,77 @@
-<template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        y
-      </h1>
-      <h2 class="subtitle">
-        slp shot inc landing
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+<template lang="pug">
+main
+  section.main-section
+    .container.container--big
+      .text-logo
+        h2 slp shot, inc
+        p Steve Lee Parsons
+      .text-container
+        h1
+          span Specialist in
+          |Vintage and modern guns, Luxury Mechanical Watches
+        h4 FEDERAL AND COMMONWEALTH OF PENNSYLVANIA LICENSED TO SELL, BUY AND TRANSFER FIREARMS
+
+  section.about-section
+    h2 about us
+    .container
+      .text-container
+        .logo
+        .wrap
+          h3 Welcome to Steve Lee ParsonS Shop
+          p I have been a Firearms and Watch Collector since the age of 10 years. <br /> We have a Winchester Rifle that has been passed down through five generations of my Family. I have always been an avid target shooter, and, as a US Army Veteran, I have had many years of training in the safe handling of firearms.
+          p I have watches that date back to the 1880’s.  I have always had a fascination with machines, and enjoy working on restoration of both watches and firearms. I have decided to turn my life’s interest into a new business venture.
+          p This is why I went through the process of applying for both Federal and State Licenses to sell firearms.
+
+  section.products-section
+    .container
+      .products-list
+        .column
+          img(
+            src="/img/rifles.jpg"
+            alt="Dealer in Rifles, Shotguns, Handguns")
+          h5 Dealer in Rifles, Shotguns, Handguns
+          p “Guns kill people like spoons made Rosie O’Donnell Fat”
+        .column
+          img(
+            src="/img/training.jpg"
+            alt="Qualified firearms training")
+          h5 Qualified firearms training (Outdoor Firing Range)
+        .column
+          img(
+            src="/img/watches.jpg"
+            alt="Dealer in Luxury/Vintage  Mechanical Watches")
+          h5 Dealer in Luxury/Vintage  Mechanical Watches
+          p “The Watch is to Time as the Brain is to the Mind”
+  footer.main-footer
+    .container
+      .column
+        h6 Contact Us
+        ul
+          li Downingtown, Pennsylvania
+          li
+            |Tel.
+            a(href="tel:610-613-3451") 610-613-3451
+          li
+            |E-mail.
+            a(href="mailto:slparsons42@outlook.com") slparsons42@outlook.com
+      .column
+        h6 Hours of Operation
+        ul
+          li Monday - Saturday: 9am - 8pm
+          li Sunday: 10am - 6pm
+      .column
+        .logo.logo--small
+        p.copyright ©{{ year }} SLP SHOT INC. All rights reserved.
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data () {
+    return {
+      year: new Date().getFullYear()
+    }
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style lang="scss"></style>
